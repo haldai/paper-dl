33b0122e368c719ece9912b0dc035f939a57f052
murongxixi <murongxixi@yeah.net>
2019-12-11 16:31:05 +0800

    update scripts

--- install.sh
+++ install.sh
@@ -1,3 +1,8 @@
 ./uninstall.sh
-mkdir -p ~/.local/bin
-ln -s $(readlink -f paper-dl) ~/.local/bin/paper-dl
+
+mkdir -p $HOME/.config
+cp paper-dl.conf $HOME/.config/
+
+mkdir -p $HOME/.local/bin # remember to add $HOME/.local/bin to $PATH
+ln -s $(readlink -f paper-dl) $HOME/.local/bin/paper-dl
+ln -s $(readlink -f paper-dl-util) $HOME/.local/bin/paper-dl-util
--- uninstall.sh
+++ uninstall.sh
@@ -1,1 +1,3 @@
-rm ~/.local/bin/paper-dl
+rm -f $HOME/.local/bin/paper-dl
+rm -f $HOME/.local/bin/paper-dl-util
+rm -f $HOME/.config/paper-dl.conf
